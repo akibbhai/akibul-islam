94fccfa0b46005ec30fcd3ef89cb4a8ac64693bd
Muhammed Akibul Islam <mdakibb786@gmail.com>
2021-07-17 23:28:43 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+// MixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+// Hemburger Menu
+
+function openMenu() {
+    document.getElementById('navbar').style.height = "100vh";
+}
+
+function closeMenu() {
+    document.getElementById('navbar').style.height = "0";
+}
+
+// Smooth Scroll
+
+$(function(){
+    $('.menu-item a, .scroll-down a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+});
